d24ec74cda63455fc85646c1ed0c35ee274ae684
eleith <online+git@eleith.com>
2016-05-29 16:29:02 -0700

    first empty project commit

---
+++ app/src/main/java/com/eleith/calchoochoo/ScheduleExplorerActivity.java
@@ -0,0 +1,13 @@
+package com.eleith.calchoochoo;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class ScheduleExplorerActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.schedule_explorer_activity);
+    }
+}
