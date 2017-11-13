0a6d8fdab52bf94c6aff47d73a20b6b76d3dd8ea
eleith <online+git@eleith.com>
2017-11-12 16:08:39 -0800

    update database and build for studio 3.0

--- app/src/main/java/com/eleith/calchoochoo/data/ChooChooDatabase.java
+++ app/src/main/java/com/eleith/calchoochoo/data/ChooChooDatabase.java
@@ -6,8 +6,8 @@ import com.readystatesoftware.sqliteasset.SQLiteAssetHelper;
 
 public class ChooChooDatabase extends SQLiteAssetHelper {
 
-  private static final String DATABASE_NAME = "caltrain2.db";
-  private static final int DATABASE_VERSION = 2;
+  private static final String DATABASE_NAME = "caltrain-10.01.2017.db";
+  private static final int DATABASE_VERSION = 4;
 
   public ChooChooDatabase(Context context) {
     super(context, DATABASE_NAME, null, DATABASE_VERSION);
